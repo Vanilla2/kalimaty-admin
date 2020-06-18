a680eaae897e7d9008a7feee9b5a95970fbecf5a
Vanilla <solmarians05@gmail.com>
2020-06-18 20:48:53 +0300

    Updates

---
+++ precache-manifest.774e7a14b94950b0abe22c1c9189497e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e206d32e841f76023696e6cb50ad23e",
+    "url": "/kalimaty-admin/index.html"
+  },
+  {
+    "revision": "14d79a408cbbbc78ed75",
+    "url": "/kalimaty-admin/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "38a66e820bf32b4f2bb3",
+    "url": "/kalimaty-admin/static/css/main.6f097817.chunk.css"
+  },
+  {
+    "revision": "14d79a408cbbbc78ed75",
+    "url": "/kalimaty-admin/static/js/2.70bb67f5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/kalimaty-admin/static/js/2.70bb67f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a66e820bf32b4f2bb3",
+    "url": "/kalimaty-admin/static/js/main.c687cbc6.chunk.js"
+  },
+  {
+    "revision": "3a28c46bac2e67de4e88",
+    "url": "/kalimaty-admin/static/js/runtime-main.2f180634.js"
+  }
+]);
